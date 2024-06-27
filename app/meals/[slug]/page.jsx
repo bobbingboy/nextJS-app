8f755bab7d893fb4c820368661192c7ce6ae1079
bobbingboy <zo17751991@hotmail.com>
2024-06-27 21:46:22 +0800

    first commit on mac (#3)

--- app/meals/[slug]/page.jsx
+++ app/meals/[slug]/page.jsx
@@ -1,6 +1,10 @@
 function MealsSlugPage(props) {
   const { params } = props;
 
+  function testForSlug(slug) {
+    return slug === params.slug;
+  }
+
   return <div>{`meal-${params.slug}`}</div>;
 }
 
